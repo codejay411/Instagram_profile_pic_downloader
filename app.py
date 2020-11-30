4bd3a85043fc81104478808e1910114bb3ad3f15
codejay411 <jaypr202@gmail.com>
2020-11-30 15:29:30 +0530

    commit

--- app.py
+++ app.py
@@ -1,38 +1,65 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+from flask import request
+import os
 import random
 import os.path
 import urllib.request as urllib2
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import sys
+import logging
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
-def hello_world():
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/insta', methods=["POST"])
+def insta():
     insta_url = 'https://www.instagram.com/'
-    if (request.method == "POST"):
-        mydict=request.form
-        insta_username=str(mydict['name'])
+    mydict=request.form
+    insta_username=str(mydict['name'])
+    # url
+    url=insta_url+insta_username
+    r=requests.get(url)
+    source=r.content
+    # source = urllib2.urlopen(url)
+    soup = bs(source, 'lxml')
+    # print(soup)
+    #image_link=soup.find_all("meta")[15].get('content')
+    image_link=soup.find("meta",  property="og:image")["content"]
+    # render on the template
+    return render_template('index.html', image_link=image_link)
 
-        # url
-        url=insta_url+insta_username
+    # insta_url = 'https://www.instagram.com/'
+    # if (request.method == "POST"):
+    #     mydict=request.form
+    #     insta_username=str(mydict['name'])
 
-        r=requests.get(url)
-        source=r.content
-        # source = urllib2.urlopen(url)
-        soup = bs(source, 'lxml')
-        # print(soup)
-        image_link=soup.find_all("meta")[15].attrs['content']
+    #     # url
+    #     url=insta_url+insta_username
+
+    #     r=requests.get(url)
+    #     source=r.content
+    #     # source = urllib2.urlopen(url)
+    #     soup = bs(source, 'lxml')
+    #     # print(soup)
+    #     image_link=soup.find_all("meta")[15].attrs['content']
         
         
 
         # render on the template
-        return render_template('index.html', image_link=image_link)
-    return render_template('index.html')
+    #     return render_template('index.html', image_link=image_link)
+    # return render_template('index.html')
 
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run()
